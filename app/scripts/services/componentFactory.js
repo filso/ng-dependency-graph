ed2d5e981163337f7025baa445cfd2eba06acfc1
Filip Sobczak <filsob@gmail.com>
2015-06-04 16:16:40 +0100

    formatting

--- app/scripts/services/componentFactory.js
+++ app/scripts/services/componentFactory.js
@@ -3,22 +3,16 @@
 angular.module('ngDependencyGraph')
   .factory('componentFactory', function(Component) {
 
-
     function createComponents(rawNodes) {
-
-      // debugger;
-
       var nodes = _.map(rawNodes, function(rawNode) {
         return new Component(rawNode);
       });
 
       _.each(nodes, function(node1) {
 
-
         var node1Deps = _.filter(nodes, function(item) {
           return _.contains(node1._data.deps, item._data.name);
         });
-
 
         _.each(node1Deps, function(node2) {
           node1.linkDep(node2);
@@ -27,7 +21,6 @@ angular.module('ngDependencyGraph')
 
       });
       return nodes;
-
     }
 
     return {
