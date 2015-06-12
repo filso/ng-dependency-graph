c4c46214d631e323d98ff3cbd4930d7e08face8f
Filip Sobczak <filsob@gmail.com>
2015-06-12 02:59:59 +0100

    inject inspected app's dependency tree

--- app/scripts/app/MainCtrl.js
+++ app/scripts/app/MainCtrl.js
@@ -1,63 +1,82 @@
 'use strict';
 
 angular.module('ngDependencyGraph')
-  .controller('MainCtrl', function($scope, $timeout, dev, Graph, Const, currentView) {
-
+  .controller('MainCtrl', function($scope, $timeout, dev, Graph, Const, currentView, appDeps) {
+    var self = this;
     $scope.currentView = currentView;
 
-    var rawData = sampleAppData.ngArchitecture;
-    _.each(rawData.modules, function(module) {
-        module.type = 'module';
-      
-        _.each(module.components, function(com) {
-            com._module = module;
-        });
+    var rawData;
+
+    appDeps.get(function(deps) {
+      if (deps) {
+        rawData = deps;
+      } else {
+        rawData = sampleAppData.ngArchitecture;
+      }
+      init();
     });
 
-    // TODO handle angular not found / wrong version
-    if (angular.version) {
-    }
-
-    this.chooseScope = function(val) {
+    self.chooseScope = function(val) {
       currentView.setScope(val);
     };
 
-    var self = this;
+    function init() {
 
-    var allComponents = [];
-    _.each(rawData.modules, function(module) {
-      allComponents = allComponents.concat(module.components);
-    });
+      _.each(rawData.modules, function(module) {
+          module.type = 'module';
+        
+          _.each(module.components, function(com) {
+              com._module = module;
+          });
+      });
 
-    var componentsGraph = Graph.createFromRawNodes(allComponents, Const.Scope.COMPONENTS);
-    var modulesGraph = Graph.createFromRawNodes(rawData.modules, Const.Scope.MODULES);
-
-    /**
-     * Connect modules with components
-     */
-    _.each(componentsGraph.nodes, function(com) {
-      var module = _.find(modulesGraph.nodes, {name: com._data._module.name});
-      com.module = module;
-
-      module.componentsByType = module.componentsByType || {};
-      if (module.componentsByType[com.type] === undefined) {
-        module.componentsByType[com.type] = [];
+      // TODO handle angular not found / wrong version
+      if (angular.version) {
       }
-      module.componentsByType[com.type].push(com);
-
-    });
 
 
-    currentView.setGraphs(modulesGraph, componentsGraph);
 
-    // DEBUG that's for development
-    $timeout(function() {
-      var node = _.find(componentsGraph.nodes, {name: 'clView'});
-      // var node = _.find(modulesGraph.nodes, {name: 'az-ci'});
-      currentView.chooseNode(node);
-    }, 300);
+      var allComponents = [];
+      _.each(rawData.modules, function(module) {
+        allComponents = allComponents.concat(module.components);
+      });
 
-    // this.chooseScope(Const.Scope.COMPONENTS);
+      var componentsGraph = Graph.createFromRawNodes(allComponents, Const.Scope.COMPONENTS);
+      var modulesGraph = Graph.createFromRawNodes(rawData.modules, Const.Scope.MODULES);
+
+      /**
+       * Connect modules with components
+       */
+      _.each(componentsGraph.nodes, function(com) {
+        var module = _.find(modulesGraph.nodes, {name: com._data._module.name});
+        com.module = module;
+
+        module.componentsByType = module.componentsByType || {};
+        if (module.componentsByType[com.type] === undefined) {
+          module.componentsByType[com.type] = [];
+        }
+        module.componentsByType[com.type].push(com);
+
+      });
+
+
+      currentView.setGraphs(modulesGraph, componentsGraph);
+
+      // DEBUG that's for development
+      $timeout(function() {
+        var node = _.find(componentsGraph.nodes, {name: 'clView'});
+        // var node = _.find(modulesGraph.nodes, {name: 'az-ci'});
+        // currentView.chooseNode(node);
+        // currentView.setScope(Const.Scope.COMPONENTS);
+      }, 300);
+
+      currentView.setScope(Const.Scope.COMPONENTS);
+      console.log(currentView.graph);
+      console.log('ha!');
+
+      // this.chooseScope(Const.Scope.COMPONENTS);
+
+    }
 
 
   });
--- app/scripts/core/appDeps.js
+++ app/scripts/core/appDeps.js
@@ -7,19 +7,27 @@ angular.module('ngDependencyGraph')
     var _depsCache = [];
 
     // // clear cache on page refresh
-    // appContext.watchRefresh(function() {
-    //   _depsCache = [];
-    // });
+    // if (chrome.extension) { // TODO ... this isn't necessary in tab context, chrome.extension should be always defined
+    //                         // maybe abstract this in appContext?
+    //   appContext.watchRefresh(function() {
+    //     _depsCache = [];
+    //   });
+    // }
 
     return {
       get: function(callback) {
+        if (!chrome.extension) { // TODO do sth smarter... maybe load sample app?
+          callback(false);
+          return;
+        }
         chromeExtension.eval(function(window) {
-            if (window.__ngArchitecture) {
-              return '';
-              return window.__ngArchitecture.getDeps();
+            if (window.__ngDependencyGraph) {
+              console.log('meta', window.__ngDependencyGraph.getMetadata());
+              return window.__ngDependencyGraph.getMetadata();
             }
           },
           function(data) {
+            console.log(data);
             if (data) {
               _depsCache = data;
             }
