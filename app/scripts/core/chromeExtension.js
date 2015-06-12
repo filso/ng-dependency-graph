1e00ebd1035dd9b61d6ab27790414ef99e11c225
Filip Sobczak <filsob@gmail.com>
2015-06-12 02:59:28 +0100

    formatting

--- app/scripts/core/chromeExtension.js
+++ app/scripts/core/chromeExtension.js
@@ -2,17 +2,19 @@
 
 // abstraction layer for Chrome Extension APIs
 angular.module('ngDependencyGraph').value('chromeExtension', {
-  sendRequest: function (requestName, cb) {
+  sendRequest: function(requestName, cb) {
     chrome.extension.sendRequest({
       script: requestName,
       tab: chrome.devtools.inspectedWindow.tabId
-    }, cb || function () {});
+    }, cb || function() {});
   },
 
-  // evaluates in the context of a window
-  //written because I don't like the API for chrome.devtools.inspectedWindow.eval;
-  // passing strings instead of functions are gross.
-  eval: function (fn, args, cb) {
+  /**
+   * @btford:
+   * written because I don't like the API for chrome.devtools.inspectedWindow.eval;
+   * passing strings instead of functions are gross.
+   */
+  eval: function(fn, args, cb) {
     // with two args
     if (!cb && typeof args === 'function') {
       cb = args;
