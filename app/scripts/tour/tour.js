02b643e5342806bf4a79a8091d8fc5928a4a16ec
Filip Sobczak <filsob@gmail.com>
2015-06-22 18:51:43 +0100

    init tour

--- app/scripts/tour/tour.js
+++ app/scripts/tour/tour.js
@@ -4,20 +4,67 @@ angular.module('ngDependencyGraph')
   .factory('tour', function() {
     var tour = new Shepherd.Tour({
       defaults: {
-        classes: 'shepherd-theme-arrows',
-        scrollTo: true
+        classes: 'shepherd-theme-default shepherd-element shepherd-open',
+        showCancelLink: true,
+        scrollTo: false
       }
     });
 
-    tour.addStep('example-step', {
-      text: 'This step is attached to the bottom of the <code>.example-css-selector</code> element.',
-      attachTo: '.info-panel left',
-      buttons: [
-        {
+    var stepButtons = [{
           text: 'Next',
           action: tour.next
-        }
-      ]
+    }];
+
+    var steps = {
+
+      welcome: {
+        text: 'Welcome to AngularJS depedency graph browser.',
+        buttons: stepButtons
+      },
+
+      chooseScope: {
+        text: 'Switch between modules and components view...',
+        attachTo: '.choose-scope bottom',
+        buttons: stepButtons
+      },
+
+
+      ignoreModules: {
+        text: 'Use ‘Ignore’ field to hide modules you don’t want to see..',
+        attachTo: '.options__ignore left',
+        buttons: stepButtons
+      },
+
+
+      filterModules: {
+        text: '…or ‘Filter’ field to specify which modules you want to see.',
+        attachTo: '.options__filter left',
+        buttons: stepButtons
+      },
+
+      stickyNodes: {
+        text: 'If you’d like your nodes to stay where you drag them - make nodes sticky.',
+        attachTo: '.options__sticky-nodes left',
+        buttons: stepButtons
+      },
+
+      stickyNodes: {
+        text: 'You can filter components view by type.',
+        attachTo: '.trigger-components right',
+        buttons: stepButtons
+      },
+
+      search: {
+        text: 'To focus on particular component or module, use search field.',
+        attachTo: '.search right',
+        buttons: stepButtons
+      },
+
+
+    };
+
+    _.each(steps, function(step) {
+      tour.addStep(step);
     });
 
     return tour;
