9898d6346edb7bedf806c1b2298ce1ec2d8d5c5a
Filip Sobczak <filsob@gmail.com>
2015-10-06 18:18:45 +0100

    fix bug, update version

--- app/scripts/app/AppCtrl.js
+++ app/scripts/app/AppCtrl.js
@@ -10,8 +10,6 @@ angular.module('ngDependencyGraph')
       MAIN: 'scripts/main/main.html'
     };
 
-    _this.appTemplate = templates.LOADING;
-
     _this.loadSampleApp = function() {
       inspectedApp.loadSampleData();
       _this.appTemplate = templates.MAIN;
@@ -29,7 +27,6 @@ angular.module('ngDependencyGraph')
       appContext.getCookie(function(appName) {
         if (appName !== null && appName !== 'true') {
           // App enabled for this page.
-          _this.appTemplate = templates.ABOUT;
           _this.appName = appName;
           inspectedApp.loadInspectedAppData([appName]).then(function() {
             if (_this.appTemplate !== templates.MAIN) {
@@ -54,7 +51,7 @@ angular.module('ngDependencyGraph')
     } else {
       // just load sample app, not in a tab, development / test
       _this.loadSampleApp();
-      $scope.$broadcast('initMain');
+      $scope.$broadcast(Const.Events.INIT_MAIN);
     }
 
   });
